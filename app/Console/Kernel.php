7ac8ace3b4a8524a1c2479928b6719fe8ef58f23
miran <mirangirostom@gmail.com>
2022-11-24 02:57:25 +0300

    Удаления файла по времени

---
+++ app/Console/Kernel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console;
+
+use Carbon\Carbon;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->call(function () {
+            $files = DB::table('files')
+                ->where('file_retention_period_at', '<=', Carbon::today()->format('Y-m-d h:m:s'));
+
+           foreach ($files->get() as $file) {
+               Storage::delete($file->path);
+            }
+
+            $files->delete();
+        })->daily();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
