f1a36836ee796f650e48e9f04838fb6ab1da9ed9
miran <mirangirostom@gmail.com>
2022-11-24 02:50:51 +0300

    Requests

---
+++ app/Http/Requests/FileRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CheckPhpType;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Foundation\Http\FormRequest;
+
+class FileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'file' => [
+                new CheckPhpType(),
+                File::default()
+                    ->max(20000)
+            ],
+        ];
+    }
+}
